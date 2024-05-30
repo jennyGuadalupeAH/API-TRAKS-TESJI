d8152d77dd3d51e4fd50128225565687ed19ef8d
Jennifer <jennyangeleshernandez310@gmail.com>
2024-05-30 08:52:19 -0600

    inicio del proyecto

---
+++ src/main/java/com/tesji/api/apitrakstesji/ApiTraksTesjiApplication.java
@@ -0,0 +1,13 @@
+package com.tesji.api.apitrakstesji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTraksTesjiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTraksTesjiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tesji/api/apitrakstesji/ApiTraksTesjiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tesji.api.apitrakstesji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTraksTesjiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
