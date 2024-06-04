6d0d90eb95fc5a4b48c8e69a609578b1910340ab
Jennifer <jennyangeleshernandez310@gmail.com>
2024-06-04 10:04:36 -0600

    ADD: Inyectar un objeto de la clase crudApiTracksRepository para las operaciones CRUD

--- src/main/java/com/tesji/api/apitrakstesji/service/ApiTracksImplement.java
+++ src/main/java/com/tesji/api/apitrakstesji/service/ApiTracksImplement.java
@@ -3,36 +3,38 @@ package com.tesji.api.apitrakstesji.service;
 import com.tesji.api.apitrakstesji.model.DatosApiTraks;
 import com.tesji.api.apitrakstesji.repository.CrudApiTracksRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Optional;
 
-public class ApiTracksServiceImplement implements ApiTraksService {
+@Service
+public class ApiTracksImplement implements ApiTraksService {
 
     @Autowired
-    CrudApiTracksRepository crudApiTraksRepository;
+    CrudApiTracksRepository crudApiTracksRepository;
 
     @Override
     public ArrayList<DatosApiTraks> mostrarTracks() {
-        return (ArrayList<DatosApiTraks>) crudApiTraksRepository.findAll();
+        return (ArrayList<DatosApiTraks>) crudApiTracksRepository.findAll();
     }
 
     @Override
     public Optional<DatosApiTraks> buscarTraksPorId(long id) {
-        return crudApiTraksRepository.findById(id);
+        return crudApiTracksRepository.findById(id);
     }
 
     @Override
     public DatosApiTraks registrarTraks(DatosApiTraks cancion) {
-        return crudApiTraksRepository.save(cancion);
+        return null;
     }
 
     @Override
-    public boolean eliminarTrack(long id) {
+    public boolean eliminartrack(long id) {
         return false;
     }
 }
-// Inyectar un objeto de la clase crudApiTraksRepository para las operaciones CRUD
+// Inyectar un objeto de la clase crudApiTracksRepository para las operaciones CRUD
 //objeto normal
 
 
