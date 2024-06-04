f8ec1e603840024c95adbd59a88da6e8b7ae514e
Jennifer <jennyangeleshernandez310@gmail.com>
2024-06-04 09:24:12 -0600

    ADD: metodos abstractos de mostrar,registrar, actualizar y eliminarid

--- src/main/java/com/tesji/api/apitrakstesji/service/ApiTraksService.java
+++ src/main/java/com/tesji/api/apitrakstesji/service/ApiTraksService.java
@@ -1,5 +1,14 @@
 package com.tesji.api.apitrakstesji.service;
 
-//Definir los metodos
+import com.tesji.api.apitrakstesji.model.DatosApiTraks;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+//Definir  metodos abstractos para implementar en los end-points"
 public interface ApiTraksService {
+    ArrayList<DatosApiTraks> mostrarTracks(); //metodos para mostrar los tracks
+    Optional<DatosApiTraks> buscarTraksPorId(long id);
+    DatosApiTraks registrarTraks(DatosApiTraks cancion); //Registrar y actualizar
+    boolean eliminarTrack(long id);
 }
