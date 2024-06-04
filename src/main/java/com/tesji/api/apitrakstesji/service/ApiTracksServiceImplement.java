a8ccb33210b6be41972e829f33b2f65b87da86fa
Jennifer <jennyangeleshernandez310@gmail.com>
2024-06-04 09:22:11 -0600

    ADD: implementacion de los metodos

--- src/main/java/com/tesji/api/apitrakstesji/service/ApiTracksServiceImplement.java
+++ src/main/java/com/tesji/api/apitrakstesji/service/ApiTracksServiceImplement.java
@@ -1,2 +1,28 @@
-package com.tesji.api.apitrakstesji.service;public class ApiTracksServiceImplement {
+package com.tesji.api.apitrakstesji.service;
+
+import com.tesji.api.apitrakstesji.model.DatosApiTraks;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+public class ApiTracksServiceImplement implements ApiTraksService {
+    @Override
+    public ArrayList<DatosApiTraks> mostrarTracks() {
+        return null;
+    }
+
+    @Override
+    public Optional<DatosApiTraks> buscarTraksPorId(long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public DatosApiTraks registrarTraks(DatosApiTraks cancion) {
+        return null;
+    }
+
+    @Override
+    public boolean eliminarTrack(long id) {
+        return false;
+    }
 }
