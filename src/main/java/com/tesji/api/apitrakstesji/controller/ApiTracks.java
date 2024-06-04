d0d01a311c39a4c1e45e40d91f132710e83d2188
Jennifer <jennyangeleshernandez310@gmail.com>
2024-06-04 07:57:11 -0600

    ADD:metodo de prueba Get en modo de desarrollo para comprobar las peticiones

--- src/main/java/com/tesji/api/apitrakstesji/controller/ApiTracks.java
+++ src/main/java/com/tesji/api/apitrakstesji/controller/ApiTracks.java
@@ -1,2 +1,20 @@
-package com.tesji.api.apitrakstesji.controller;public class ApiTracks {
+package com.tesji.api.apitrakstesji.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//clase que define el Acceso a la API mediante URL
+// y tambien los end-pints(peticiones http)
+@RestController //control de peticiones "la clase es una api REST"
+@RequestMapping("/api-traks") //Mapear las respuestas
+public class ApiTracks {
+    @GetMapping("get-prueba")
+    public String Pruebaget(){
+        return "Funciona!! Bienvenido a la API TRANKS TESJI";
+
+
+
+    }
 }
+
