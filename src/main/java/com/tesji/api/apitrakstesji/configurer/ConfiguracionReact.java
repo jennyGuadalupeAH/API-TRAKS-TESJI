996619fdf0ec7030491485ab0b6088c8a588a369
Jennifer <jennyangeleshernandez310@gmail.com>
2024-06-10 15:28:31 -0600

    ADD: Agregar configuración de CORS para permitir acceso del frontend

---
+++ src/main/java/com/tesji/api/apitrakstesji/configurer/ConfiguracionReact.java
@@ -0,0 +1,25 @@
+package com.tesji.api.apitrakstesji.configurer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class ConfiguracionReact {
+    @Bean
+    public WebMvcConfigurer MiConfiguracionReact(){
+        return  new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000") // Dirección de nuestro frontend React
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+
+            }
+        };
+    }
+}
